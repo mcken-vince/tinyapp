1111abb492626c7b5c3c11f3e98d92fee8c4dcaf
Vincent McKen <mcken.vince@gmail.com>
2021-08-02 15:55:28 +0000

    create views folder & urls_index.ejs file

--- express_server.js
+++ express_server.js
@@ -9,17 +9,21 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/urls", (req, res) => {
+  res.render("urls_index", urlDatabase);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
