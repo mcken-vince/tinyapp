58eb7e4a21c64218f10954ca0ce68fdf92b84ac4
Vincent McKen <mcken.vince@gmail.com>
2021-08-02 16:18:52 +0000

    changed urlDatabase object to have key=urls, working now

--- express_server.js
+++ express_server.js
@@ -4,9 +4,10 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
-const urlDatabase = {
+const urlDatabase = {urls: {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+  }
 };
 
 app.get("/urls", (req, res) => {
