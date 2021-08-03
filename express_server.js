bea10c5bba4078a75bc823abfc64d589b331db2c
Vincent McKen <mcken.vince@gmail.com>
2021-08-03 16:16:15 +0000

    username set as cookie, edit button not working

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ app.set("view engine", "ejs");
 
 const cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}), cookieParser());
+app.use( cookieParser(), bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -24,16 +24,16 @@ const generateRandomString = () => {
 };
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies.username };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  res.render("urls_new", req.cookies);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies.username };
   res.render("urls_show", templateVars);
 });
 
@@ -58,14 +58,16 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 // change longURL for existing shortURL
 app.post("/urls/:shortURL", (req, res) => {
-  // console.log(`${urlDatabase[req.params.shortURL]} changed to ${req.body.longURL}`)
+  console.log(`${urlDatabase[req.params.shortURL]} changed to ${req.body.longURL}`)
   urlDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect("/urls");
 });
 
 // sent here by login button
 app.post("/login", (req, res) => {
-  console.log(cookieParser);
+  res.cookie('username', req.body.username);
+  
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
