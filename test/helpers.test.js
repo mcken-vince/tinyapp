6644a841d7da8536f6366e43c40ac35fcf6e927e
Vincent McKen <mcken.vince@gmail.com>
2021-08-04 23:34:41 +0000

    write test for generateRandomString

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -1,2 +1,18 @@
 const assert = require('chai').assert;
 const { generateRandomString, propSearch, urlSearch } = require('../helpers');
+
+describe('#generateRandomString', () => {
+  it('should generate a string 6 characters long', () => {
+    const string = generateRandomString();
+    assert.strictEqual(string.length, 6);
+    assert.isString(string);
+  });
+});
+
+describe('#propSearch', () => {
+
+});
+
+describe('#urlSearch', () => {
+
+});
